81772608c6f3d019d73f37ca281d6c5c1ffd70d6
PauloLuedy <pauloluedyads@gmail.com>
2021-01-06 03:57:25 -0300

    postgres get post

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -4,16 +4,18 @@ const port = 3001
 const bodyParser = require('body-parser')
 const MongoDb = require('./src/db/strategies/mongodb/mongoDbStrategy')
 const HeroSchema = require('./src/db/strategies/mongodb/schemas/heroSchema')
+const HeroiSchema = require('./src/db/strategies/postgres/schemas/heroiSchema')
+const PostgresStrategy = require('./src/db/strategies/postgres/postgresSQLStrategy');
+
 const Context = require('./src/db/strategies/base/contextStrategy')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-
     res.send('Hello World!')
 })
-
+//mongodb
 app.post('/herois', async (req, res) => {
     const connection = MongoDb.connect()
     context = new Context(new MongoDb(connection, HeroSchema))
@@ -25,6 +27,22 @@ app.get('/herois', async (req, res) => {
     const connection = MongoDb.connect()
     context = new Context(new MongoDb(connection, HeroSchema))
     const result = await context.read()
+    res.json(result)
+});
+
+//postgress
+app.post('/herois2', async (req, res) => {
+    const connection = await PostgresStrategy.connect()
+    const context = await PostgresStrategy.defineModel(connection, HeroiSchema)
+    const result = await context.create(req.body)
+    res.json(result)
+})
+
+app.get('/herois2', async (req, res) => {
+    const all = { attributes: ['nome', 'poder', 'id'] };
+    const connection = await PostgresStrategy.connect()
+    const context = await PostgresStrategy.defineModel(connection, HeroiSchema)
+    const result = await context.findAll(all)
     res.json(result)
 });
 
